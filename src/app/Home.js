672ac69ecaf9afb0797f74b8a76f833116d65f41
C-DYoung <rbrbdk12@gmail.com>
2024-05-25 15:08:55 +0900

    feat : Home 링크 기능 추가

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,6 +1,8 @@
+"use client"
+import Link from "next/link";
 
 export function Home() {
   return (
-    <h2> home, home page</h2>
+    <h2> <Link href="/">home</Link>home, home page</h2>
   );
 }
---
+++ src/app/signup/page.js
@@ -0,0 +1,5 @@
+export default function singUp() {
+    return(
+        <h2>Hello sign up page</h2>
+    );
+};
